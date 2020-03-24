89aa763446cc2efd2760ad57581f4d76794ff650
Alix FDM <boujour@alixfdm.fr>
2020-03-24 09:55:18 -0300

    Add some animation on nav links

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 (function() {
   const burger = document.querySelector(".burger");
   const navbar = document.querySelector("nav ul");
+  const navbarLinks = document.querySelectorAll("nav ul li");
 
   burger.addEventListener("click", () => {
     navbar.classList.toggle("toggle-navbar");
+
+    navbarLinks.forEach((link, index) => {
+      // Handmade toggle :)
+      if (!link.style.animation) {
+        link.style.animation = `smoothTranslate ${(index + 1) *
+          0.3}s ease-in-out forwards`;
+      } else link.style.animation = "";
+    });
   });
 })();
