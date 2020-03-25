cb0ff395ac65fc653648239fe9262e3f10bc72ad
Alix FDM <boujour@alixfdm.fr>
2020-03-24 23:30:37 -0300

    animation on the burger menu

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
   const burger = document.querySelector(".burger");
   const navbar = document.querySelector("nav ul");
   const navbarLinks = document.querySelectorAll("nav ul li");
+  const burgerLines = document.querySelectorAll(".burger .lines");
 
   burger.addEventListener("click", () => {
     navbar.classList.toggle("toggle-navbar");
+    burger.classList.toggle("toggle");
 
     navbarLinks.forEach((link, index) => {
       // Handmade toggle :)
       if (!link.style.animation) {
-        link.style.animation = `smoothTranslate ease-out ${(index + 3) *
-          0.3}s forwards`;
+        link.style.animation = `smoothTranslate ease ${(index + 1) *
+          0.2}s forwards`;
       } else link.style.animation = "";
     });
   });
